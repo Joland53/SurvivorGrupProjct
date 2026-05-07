c35ecf5b612d0fde15fe44643b4bd6c49c44f4c4
Joland53 <berbelnajasjj@gmail.com>
2026-05-07 13:17:39 +0200

    Actualizar Binarios de FMOD y Visual Studio
    He actualizado y forzado a subir los binarios de Windows de FMOD para evitar problemas con los pull. Se han añadido también configuraciones con Visual Studio

---
+++ Source/SurvivorGrupProjct/SurvivorGrupProjct.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "SurvivorGrupProjct.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, SurvivorGrupProjct, "SurvivorGrupProjct" );
