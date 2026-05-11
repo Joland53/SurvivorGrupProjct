bfff27e78322d620c778eeefad8aaf9bfeed9124
Joland53 <berbelnajasjj@gmail.com>
2026-05-11 10:54:27 +0200

    Migracion de Proyecto a UE 5.5
    Se ha migrado el proyecto a al version 5.5. Hemos eliminado las configuraciones en Visual Studio para que solo funcione en Blueprints y no haya problemas.

--- Source/SurvivorGrupProjct/SurvivorGrupProjct.cpp
+++
@@ -1,6 +0,0 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-#include "SurvivorGrupProjct.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, SurvivorGrupProjct, "SurvivorGrupProjct" );
